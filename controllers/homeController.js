42c65323ba84a739d0f5107c7becb642b6a7454b
Jennifer Lin <jennliny@brandeis.edu>
2020-06-12 01:50:07 +0800

    made the subject filter work

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -16,7 +16,7 @@ var OLDcourses = [
   }
 ];
 
-const coursesFile = require("../private/courses.json");
+const courses = require("../private/courses.json");
 //console.log(`courses.length = ${courses.length}`);
 //console.log(JSON.stringify(courses[0],null,2));
 
@@ -30,13 +30,36 @@ exports.showForum = (req, res) => {
 
 exports.showCourses = (req, res) => {
   res.render("courses", {
-    offeredCourses: OLDcourses
+    courses: [],
+    term: "Fall19-Fall20",
+    department:"any",
   });
 };
 
 exports.showSelectedCourses = (req, res) => {
-  res.render("courses");
-}
+
+  let subject = req.body.subject
+
+  const selected_courses =
+      courses.filter(course =>
+        ( (! course['independent_study'])
+        &&
+        ( (!subject) || course['subject']==subject)
+        //&&
+        //(!req.body.term || course['term']==req.body.term)
+      ))
+
+
+  selected_courses.sort(
+    (c1,c2) => (c2.enrolled-c1.enrolled)
+  )
+
+  console.log("faculty = ")
+
+  res.render("courses", {
+    courses: selected_courses,
+  });
+};
 
 exports.showSignUp = (req, res) => {
   res.render("contact");
