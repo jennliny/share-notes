2c264954bf6343ad1742addfdc3d2a6f75a4eb42
Yi-WenTseng <ytseng@brandeis.edu>
2020-06-28 02:28:31 -0400

    change editProfile layout

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     'googleAuth' : {
         'clientID': '450134845444-t6qjj3n28duaicmsqeveokh8p2gjeahu.apps.googleusercontent.com',
         'clientSecret'  : 'yo8MC3Isnrg78dOjWpPhZJrC',
-        'callbackURL'   : 'https://young-retreat-22857.herokuapp.com/login/authorized'
+        'callbackURL'   : 'https://share-notes.herokuapp.com/login/authorized'
         //'callbackURL': 'https://hidden-brook-99342.herokuapp.com/'
         //'callbackURL':	'https://boiling-ridge-25440.herokuapp.com/login/authorized'
     }
